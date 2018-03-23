de2026cd61a5b607cfd5a4d67f62fd0f6539f0e6
Tyler Earls <tyler.a.earls@gmail.com>
2018-03-23 16:25:45 -0500

    finished 2nd and 3rd task

--- pixart.js
+++ pixart.js
@@ -3,9 +3,16 @@
 // Use jQuery to select the element and add an event listener
 
 $("#set-color").on("click", function (event) {
-	// select square and set its color equal to the input color
+	// select brush and set its color equal to the input color
 	// use val() to read the value of the input
 	$(".brush").css("background-color", $("input").val());
 	// to prevent page from refreshing automatically
 	event.preventDefault();
 })
+
+for (let i = 0; i < 20; i++){
+	// create 20 divs
+	// add .square class
+	// append to body
+	$("<div>").addClass("square").appendTo("body");
+}
