c581bc863425f3b1d94aaab1a8d232c3e041e2e1
Tyler Earls <tyler.a.earls@gmail.com>
2018-03-23 16:21:35 -0500

    finished first task

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+// When I click the "Set Color" button, it should change 
+// the color of the "brush" box to the color I specify in the input field.
+// Use jQuery to select the element and add an event listener
 
+$("#set-color").on("click", function (event) {
+	// select square and set its color equal to the input color
+	// use val() to read the value of the input
+	$(".brush").css("background-color", $("input").val());
+	// to prevent page from refreshing automatically
+	event.preventDefault();
+})
